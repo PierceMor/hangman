4c3974942bb376c71afa7826135014f9f292981f
Pierce <piercemorrill91@gmail.com>
2018-02-16 11:17:07 -0600

    doing work

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var words = [
+    "Vader", ]
+
+var word = "";
+var wins = 0;
+var guesses = [""];
+var lettersUsed = [""];
+
+word = words[Math.floor(Math.random())*words.length];
+
+    document.addEventListener ("keyup", function(event) {
+        if ( event == "word" ) {
+            var word = document.getElementByDiv("word");
+            innerHTML = "v"
+        }
+
+        for (i in word);
+        
+        console.log(event.key)
+    });
