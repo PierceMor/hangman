44a9b94aeefb8a7998e0c7ea93cf253d53615ee2
Pierce <piercemorrill91@gmail.com>
2018-02-16 20:02:14 -0600

    long night

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,69 @@
 var words = [
-    "Vader", ]
+    "Vader",
+    "Sith",
+    "Jedi",
+    "Bane", ];
 
-var word = "";
+var theWord = "";
+// empty array to store guesses
+var answerWord = [];
 var wins = 0;
-var guesses = [""];
+var guesses = 15;
 var lettersUsed = [""];
 
-word = words[Math.floor(Math.random())*words.length];
 
+//chooses a word at random
+
+function computerChoice() {
+
+var randoo = Math.floor((Math.random() * words.length) + 1);
+
+    theWord = words[randoo];
+
+    console.log(theWord);
+}
+
+computerChoice();
+
+
+//display current word in "Guesses"
+
+   //Answer Array
+   // answerWord = [];
+   //  for ( var i = 0; i < word.length; i++ ) {
+   //      answerWord[i] = "_"; 
+  //   }
+
+  //   document.getElementById("theWord").innerHTML = answerWord.join(" ");
+
+   // document.onkeyup = function (event) {
+  //      theWord = event.key.toUpperCase();
+   // }
+
+
+        //IDK
     document.addEventListener ("keyup", function(event) {
-        if ( event == "word" ) {
-            var word = document.getElementByDiv("word");
-            innerHTML = "v"
-        }
-
-        for (i in word);
+      if ( event ===  ) {
+            
+            
+            console.log(event.key);
         
-        console.log(event.key)
-    });
+    }});
+        // broken loop
+       // for (i in word; i < word.length; i++ );{}
+
+        
+
+      // console.log(event.key)
+   // });
+
+//display remaining guess
+
+//show wins 
+
+//log user key commands
+
+    //keep track of correct answers
+
+    //keep track of wrong answers 
+        //alert users if wrong command is already logged 
